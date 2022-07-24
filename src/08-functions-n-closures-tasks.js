6f743e823f8b1561d24eaa2e57fce723b232949e
Bella Makhova <rgvder@gmail.com>
2022-07-24 14:25:57 +0300

    feat: 91 passing

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -95,8 +95,10 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const result = func();
+
+  return () => result;
 }
 
 
@@ -115,8 +117,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    if (attempts > 0) {
+      try {
+        return func();
+      } catch (e) {
+        return retry(func, attempts - 1)();
+      }
+    }
+
+    return 0;
+  };
 }
 
 
@@ -143,8 +155,16 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...x) => {
+    const y = x.map((item) => JSON.stringify(item)).join(',');
+    logFunc(`${func.name}(${y}) starts`);
+
+    const res = func(...x);
+
+    logFunc(`${func.name}(${y}) ends`);
+    return res;
+  };
 }
 
 
@@ -161,8 +181,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 
@@ -183,8 +203,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+
+  return () => {
+    count += 1;
+    return count;
+  };
 }
 
 
